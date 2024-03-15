6780c17a7c43b03cd9488c14435af1aca0dd2cd4
Vijay Singh <vijaysingh3225@gmail.com>
2024-03-15 03:54:07 -0400

    Many changes including addition of contact inputs

--- ProjectSlideshow.js
+++ ProjectSlideshow.js
@@ -1,30 +1,50 @@
-const family1 = document.querySelector("#family1");
-const family2 = document.querySelector("#family2");
-const family3 = document.querySelector("#family3");
-const family4 = document.querySelector("#family4");
-const family5 = document.querySelector("#family5");
-const family6 = document.querySelector("#family6");
-const display = document.querySelector("#slideNumber");
-const imgURLs = ["Images/Family1.jpg","Images/Family2.jpg","Images/Family3.jpg","Images/Family4.jpg","Images/Family5.jpg","Images/Family6.jpg"]
-const slideShowCaption = document.querySelector("#slideshow-caption");
-const slideShow = document.querySelector("#prjct-slideshow");
+const P1Family1 = document.querySelector("#p1-family1");
+const P1Family2 = document.querySelector("#p1-family2");
+const P1Family3 = document.querySelector("#p1-family3");
+const P1Family4 = document.querySelector("#p1-family4");
+const P1Family5 = document.querySelector("#p1-family5");
+const P1Family6 = document.querySelector("#p1-family6");
 
-family1.onclick = function() {
-    slideShow.style.backgroundImage = `url('${imgURLs[0]}')`;
-    slideShowCaption.innerText = "Saroj and Shyamsundar"
+const P2Family1 = document.querySelector("#p2-family1");
+const P2Family2 = document.querySelector("#p2-family2");
+const P2Family3 = document.querySelector("#p2-family3");
+const imgURLs = ["Images/Family1.jpg","Images/Family2.jpg","Images/Family3.jpg","Images/Family4.jpg","Images/Family5.jpg","Images/Family6.jpg"]
+const P1SlideShowCaption = document.querySelector("#p1-slideshow-caption");
+const P2SlideShowCaption = document.querySelector("#p2-slideshow-caption");
+const P1SlideShow = document.querySelector("#p1-prjct-slideshow");
+const P2SlideShow = document.querySelector("#p2-prjct-slideshow");
+
+P1Family1.onclick = function() {
+    P1SlideShow.style.backgroundImage = `url('${imgURLs[0]}')`;
+    P1SlideShowCaption.innerText = "Saroj and Shyamsundar"
 };
-family2.onclick = function() {
-    slideShow.style.backgroundImage = `url('${imgURLs[1]}')`;
+P1Family2.onclick = function() {
+    P1SlideShow.style.backgroundImage = `url('${imgURLs[1]}')`;
+    P1SlideShowCaption.innerText = "Rekha Devi & Lakshamn Rajbhar"
 };
-family3.onclick = function() {
-    slideShow.style.backgroundImage = `url('${imgURLs[2]}')`;
+P1Family3.onclick = function() {
+    P1SlideShow.style.backgroundImage = `url('${imgURLs[2]}')`;
+    P1SlideShowCaption.innerText = "Jeeavn Lal & Rinku Devi (Rekha's Family)"
 };
-family4.onclick = function() {
-    slideShow.style.backgroundImage = `url('${imgURLs[3]}')`;
+P1Family4.onclick = function() {
+    P1SlideShow.style.backgroundImage = `url('${imgURLs[3]}')`;
+    P1SlideShowCaption.innerText = "Bharat Rajbhar & Sunita Devi  (Rekha's family)"
 };
-family5.onclick = function() {
-    slideShow.style.backgroundImage = `url('${imgURLs[4]}')`;;
+P1Family5.onclick = function() {
+    P1SlideShow.style.backgroundImage = `url('${imgURLs[4]}')`;;
+    P1SlideShowCaption.innerText = "Arjun & Soni Dev (Rekha's family)"
 };
-family6.onclick = function() {
-    slideShow.style.backgroundImage = `url('${imgURLs[5]}')`;
+P1Family6.onclick = function() {
+    P1SlideShow.style.backgroundImage = `url('${imgURLs[5]}')`;
+    P1SlideShowCaption.innerText = "Ramji Rajbhar (Rekha's family)"
+};
+P2Family1.onclick = function() {
+    P2SlideShow.style.backgroundImage = `url('${imgURLs[2]}')`;
+    slideShowCaption.innerText = "Ramji Rajbhar (Rekha's family)"
+};
+P2Family2.onclick = function() {
+    P2SlideShow.style.backgroundImage = `url('${imgURLs[3]}')`;
+};
+P2Family3.onclick = function() {
+    P2SlideShow.style.backgroundImage = `url('${imgURLs[4]}')`;
 };
