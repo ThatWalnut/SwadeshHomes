ace930829255af2c531363414ae09a320cf44f71
Vijay Singh <vijaysingh3225@gmail.com>
2024-03-19 22:28:27 -0400

    start of gallery

---
+++ Gallery.js
@@ -0,0 +1,4 @@
+const rightButton = document.querySelector("#gallery-right-button");
+const leftButton = document.querySelector("#gallery-left-button");
+const gallerySlide = document.querySelector("#gallery-slide");
+
